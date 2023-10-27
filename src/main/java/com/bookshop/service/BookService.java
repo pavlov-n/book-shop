65500c371de52ea7a6bc8b8031790dfb91d7830b
Mykyta Pavlov <pavlov2021mf12@student.karazin.ua>
2023-10-27 14:03:02 +0300

    Added pagination, sorting and swagger

--- src/main/java/com/bookshop/service/BookService.java
+++ src/main/java/com/bookshop/service/BookService.java
@@ -2,12 +2,13 @@ package com.bookshop.service;
 
 import com.bookshop.dto.BookDto;
 import com.bookshop.dto.CreateBookRequestDto;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
--- src/main/java/com/bookshop/service/impl/BookServiceImpl.java
+++ src/main/java/com/bookshop/service/impl/BookServiceImpl.java
@@ -7,6 +7,8 @@ import com.bookshop.mapper.BookMapper;
 import com.bookshop.model.Book;
 import com.bookshop.repository.BookRepository;
 import com.bookshop.service.BookService;
+
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,8 +26,8 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll().stream()
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable).stream()
                 .map(bookMapper::toDto)
                 .toList();
     }
