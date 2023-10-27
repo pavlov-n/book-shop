d09c980048aff9dafed6b85f561c4250a4a257a0
Mykyta Pavlov <pavlov2021mf12@student.karazin.ua>
2023-10-27 15:43:47 +0300

    changes after second review

--- src/main/java/com/bookshop/controller/BookController.java
+++ src/main/java/com/bookshop/controller/BookController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @Validated
-@RequestMapping(value = "/api/books")
+@RequestMapping(value = "/books")
 public class BookController {
     private final BookService bookService;
 
