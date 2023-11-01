9e7747fc76f15d3e8d6f0312662f91b63dcd6046
Mykyta Pavlov <pavlov2021mf12@student.karazin.ua>
2023-11-01 11:12:16 +0200

    added @pageableDefault anotation

--- src/main/java/com/bookshop/controller/BookController.java
+++ src/main/java/com/bookshop/controller/BookController.java
@@ -10,6 +10,7 @@ import jakarta.validation.constraints.Positive;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -34,7 +35,7 @@ public class BookController {
     @GetMapping
     @Operation(summary = "Get all books",
             description = "Get list of books")
-    public List<BookDto> getAll(Pageable pageable) {
+    public List<BookDto> getAll(@PageableDefault Pageable pageable) {
         return bookService.findAll(pageable);
     }
 
